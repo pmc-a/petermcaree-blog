ac593cc2bbddf4e146a941adf1546956d7ccb0b0
Peter McAree <me@petermcaree.com>
2018-10-18 16:00:07 +0100

    remove boilerplate posts, modifying graphql query to ignore category

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -137,7 +137,6 @@ export const guery = graphql`
           frontmatter {
             title
             subTitle
-            category
             cover {
               children {
                 ... on ImageSharp {
